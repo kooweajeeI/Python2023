bf40dc21f36aefbdf08b443c25ae5f49069150e0
kooweajeel-it <leejw2873@naver.com>
2023-02-10 12:32:13 +0900

    10일차 기초프로그래밍 완료

---
+++ Day10/code56_pyqt_lineedit.py
@@ -0,0 +1,34 @@
+# 라인에디트 - textbox
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class MyApp(QWidget):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        self.lblResult = QLabel('test', self)
+        self.lblResult.move(20, 20)
+
+        txtInput = QLineEdit(self)
+        txtInput.setEchoMode(2)
+        txtInput.move(20, 40)
+        txtInput.textChanged[str].connect(self.onChanged)
+
+        # 필수설정
+        self.setWindowTitle('라인에디트')
+        self.setGeometry(300, 300, 300, 300)
+        self.show()
+
+    def onChanged(self, text):
+        self.lblResult.setText('입력값은 : ' + text)
+        self.lblResult.adjustSize()
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp()
+    sys.exit(app.exec_())
